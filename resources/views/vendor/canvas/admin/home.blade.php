51551532bc58a93a902ba86e59ad2cbbc5230f34
Seong Bae <seong@lnidigital.com>
2020-11-07 21:32:02 -0500

    update dashboard

--- resources/views/vendor/canvas/admin/home.blade.php
+++ resources/views/vendor/canvas/admin/home.blade.php
@@ -29,43 +29,6 @@
         </div>
     </div>
 <div class="row row-short">
-{{--    <div class="col-lg-4">--}}
-{{--        <div class="card">--}}
-{{--            <div class="card-header border-transparent">--}}
-{{--                <h3 class="card-title">Projects</h3>--}}
-
-{{--                <div class="card-tools">--}}
-{{--                    <button type="button" class="btn btn-tool" data-card-widget="collapse">--}}
-{{--                        <i class="fas fa-minus"></i>--}}
-{{--                    </button>--}}
-{{--                    <button type="button" class="btn btn-tool" data-card-widget="remove">--}}
-{{--                        <i class="fas fa-times"></i>--}}
-{{--                    </button>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--            <!-- /.card-header -->--}}
-{{--            <div class="card-body p-0">--}}
-{{--                <div class="table-responsive">--}}
-{{--                    <table class="table m-0">--}}
-{{--                        <tbody>--}}
-{{--                        @foreach(Auth::user()->projects()->orderBy('created_at','desc')->limit(5)->get() as $project)--}}
-{{--                            <tr>--}}
-{{--                                <td><a href="{{ route('projects.show', ['project'=>$project])}}">{{$project->name}}</a></td>--}}
-{{--                            </tr>--}}
-{{--                        @endforeach--}}
-{{--                        </tbody>--}}
-{{--                    </table>--}}
-{{--                </div>--}}
-{{--                <!-- /.table-responsive -->--}}
-{{--            </div>--}}
-{{--            <!-- /.card-body -->--}}
-{{--            <div class="card-footer clearfix">--}}
-{{--                <a href="{{ route('projects.create') }}" class="btn btn-sm btn-info float-left">Create a New</a>--}}
-{{--                <a href="{{ route('projects.index') }}" class="btn btn-sm btn-secondary float-right">View All</a>--}}
-{{--            </div>--}}
-{{--            <!-- /.card-footer -->--}}
-{{--        </div>--}}
-{{--    </div>--}}
 	<div class="col-lg-4">
         <div class="card">
             <div class="card-header border-transparent">
@@ -85,10 +48,12 @@
                 <div class="table-responsive">
                     <table class="table m-0">
                         <tbody>
-                        @foreach(Auth::user()->upcomingTasks as $task)
+                        @foreach(Auth::user()->upcomingTasks()->limit(10)->get() as $task)
                             <tr>
                                 <td><a href="{{ route('tasks.show', ['task'=>$task])}}">{{$task->name}}</a>
-                                    <div class="text-muted small">in blah blah</div>
+                                    @if ($task->project_id)
+                                        <div class="text-muted small">in <a href="{{route('projects.show',['project'=>$task->project])}}">{{$task->project->name}}</a></div>
+                                    @endif
                                 </td>
                                 <td class="text-right text-muted small">{{ \Carbon\Carbon::parse($task->due_on)->diffForHumans()}}</td>
                             </tr>
@@ -125,7 +90,7 @@
                 <div class="table-responsive">
                     <table class="table m-0">
                         <tbody>
-                        @foreach(Auth::user()->activeTasks()->limit(5)->get() as $task)
+                        @foreach(Auth::user()->activeTasks()->limit(10)->get() as $task)
                             <tr>
                                 <td><a href="{{ route('tasks.show', ['task'=>$task])}}">{{$task->name}}</a></td>
                                 <td class="text-right text-muted small">{{ \Carbon\Carbon::parse($task->created_at)->diffForHumans()}}</td>
@@ -158,7 +123,7 @@
 		    <div class="table-responsive">
 		      <table class="table m-0">
 		        <tbody>
-		        @foreach(Auth::user()->pastDueTasks as $task)
+		        @foreach(Auth::user()->pastDueTasks()->limit(10)->get() as $task)
 		        <tr>
 		          <td><a href="{{ route('tasks.show', ['task'=>$task])}}">{{$task->name}}</a></td>
 		          <td class="text-right text-muted small">{{ \Carbon\Carbon::parse($task->due_on)->diffForHumans()}}</td>

e7e0a1dcc9f14a39b51913ea2bb4f47ba89eaa01
Seong Bae <seong@lnidigital.com>
2020-11-30 00:06:31 -0500

    add task assign command

---
+++ app/Console/Commands/UpdateTaskAssignedCmd.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Mail\TestMail;
+use App\Models\Project;
+use App\Models\ReceivedMail;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+use App\Models\Task;
+
+class UpdateTaskAssignedCmd extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'update:taskassigned';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $tasks = Task::all();
+
+        foreach($tasks as $task)
+        {
+            if ($task->assigned_to)
+            {
+                $task->users()->save($task->assigned_to);
+                $task->save();
+            }
+
+        }
+    }
+
+}

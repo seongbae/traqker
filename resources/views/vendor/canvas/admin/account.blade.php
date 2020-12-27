8548f92b223b39ec9ac7265d74a6007b40368979
Seong Bae <seong@lnidigital.com>
2020-12-27 00:53:49 -0500

    update js

--- resources/views/vendor/canvas/admin/account.blade.php
+++ resources/views/vendor/canvas/admin/account.blade.php
@@ -407,12 +407,12 @@ if (isset($_GET['code'])) { // Redirect w/ code
 
         if ('serviceWorker' in navigator) {
             navigator.serviceWorker.register('sw.js').then(function(reg) {
-                console.log('Service Worker Registered!', reg);
+                // console.log('Service Worker Registered!', reg);
 
                 reg.pushManager.getSubscription().then(function(subscription) {
                     if (subscription === null) {
                         // Update UI to ask user to register for Push
-                        console.log('Not subscribed to push service!');
+                        // console.log('Not subscribed to push service!');
                     } else {
                         // We have a subscription, update the database
                         // console.log('Subscription object: ', subscription);

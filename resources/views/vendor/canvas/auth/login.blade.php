04f06b95fc3331eecc7ecedc83ad9cd4414069d6
Seong Bae <seong@lnidigital.com>
2021-01-04 19:48:12 -0500

    minor ui fix

--- resources/views/vendor/canvas/auth/login.blade.php
+++ resources/views/vendor/canvas/auth/login.blade.php
@@ -8,7 +8,7 @@
                 <div class="card">
                     <div class="card-header">{{ __('Login') }}</div>
 
-                    <div class="cfullard-body">
+                    <div class="card-body">
                         <form method="POST" action="{{ route('login') }}">
                             @csrf
 
